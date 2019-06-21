735c90d11ec7c9632ccbc68dc09d2fc6a3dd2619
nilesh.devdas@gmail.com <nilesh.devdas@gmail.com>
2019-06-21 17:33:26 +0530

    New Changes

---
+++ src/main/java/com/ui/tests/SelTestCase.java
@@ -0,0 +1,11 @@
+package com.ui.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelTestCase {
+
+	public static void main(String[] args) {
+
+	}
+}
--- src/test/java/com/siemens/devopsdemo/DevopsdemoApplicationTests.java
+++ src/test/java/com/siemens/devopsdemo/DevopsdemoApplicationTests.java
@@ -1,7 +1,12 @@
 package com.siemens.devopsdemo;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +16,15 @@ public class DevopsdemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testGoogle() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://bpm.vinsys.com/");
+		driver.findElement(By.id("Username")).sendKeys("nilesh.devdas@vinsys.com");
+		driver.findElement(By.id("UserPassword")).sendKeys("abcd@1234");
+		driver.findElement(By.cssSelector("body > div > div > form > button")).click();;
 	}
 
 }
